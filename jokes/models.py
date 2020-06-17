3887fa9c5afec52aea9fb9db6bfe55be63a6a785
Peng Chen <pchen0816@outlook.com>
2020-06-17 12:32:43 -0700

    done

--- jokes/models.py
+++ jokes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Content(models.Model):
+    title = models.CharField(max_length = 64)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('details', args=[str(self.id)])
--- jokes/views.py
+++ jokes/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Content
+from django.urls import reverse_lazy
 
 # Create your views here.
+class JokesListView(ListView):
+    template_name = 'list.html'
+    model = Content
+
+class JokesDetailsView(DetailView):
+    template_name = 'details.html'
+    model = Content
+
+class JokesCreateView(CreateView):
+    template_name = 'new.html'
+    model = Content
+    fields = ['title', 'author', 'body']
+
+class JokesUpdateView(UpdateView):
+    template_name = 'update.html'
+    model = Content
+    fields = ['title', 'body']
+
+class JokesDeleteView(DeleteView):
+    template_name = 'delete.html'
+    model = Content
+    success_url = reverse_lazy('list')
